efef3b41da9f7aa67c77f404bdee916038dab48b
ml3424 <ml3424@bs.amalnet.k12.il>
2025-11-14 14:51:59 +0200

    added menu to TimerActivity

--- app/src/main/java/com/example/lockin_prototype/TimerActivity.java
+++ app/src/main/java/com/example/lockin_prototype/TimerActivity.java
@@ -1,13 +1,17 @@
 package com.example.lockin_prototype;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -73,5 +77,42 @@ public class TimerActivity extends AppCompatActivity {
 
         // disable the button while the timer is running
         BtnStartTimer.setEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String st = item.getTitle().toString();
+
+        if (st.equals("Sign")) {
+            Intent si = new Intent(this, MainActivity.class);
+            startActivity(si);
+        }
+        else if (st.equals("Somthing")) {
+            // Add your 'Somthing' logic here
+        }
+        else if (st.equals("Gallery")) {
+            // Add your 'Gallery' logic here
+        }
+        else if (st.equals("Take a pic")) {
+            // Add your 'Take a pic' logic here
+        }
+        else if (st.equals("Set time period")) {
+            // Add your 'Set time period' logic here
+        }
+        else if (st.equals("Set timer")) {
+            // none
+        }
+        else if (st.equals("Ai")) {
+            // Add your 'Ai' logic here
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
