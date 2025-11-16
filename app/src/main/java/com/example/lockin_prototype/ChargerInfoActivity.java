65779a7445d1b762b3a8f9a2eca3012dfb51aedf
ml3424 <ml3424@bs.amalnet.k12.il>
2025-11-16 16:25:33 +0200

    added ChargerActivity

--- app/src/main/java/com/example/lockin_prototype/ChargerInfoActivity.java
+++ app/src/main/java/com/example/lockin_prototype/ChargerInfoActivity.java
@@ -1,9 +1,12 @@
 package com.example.lockin_prototype;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -14,11 +17,44 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ChargerInfoActivity extends AppCompatActivity {
 
+    public static ChargerReceiver batteryReceiver = new ChargerReceiver();
+    public static boolean isRegistered = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charger_info);
 
+    }
+
+    // function to register the broadcast receiver (Button 1 functionality)
+    private void registerBroadcastListener() {
+        if (!isRegistered) {
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_POWER_CONNECTED);
+            filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+            getApplicationContext().registerReceiver(batteryReceiver, filter); // using the Application Context which allows to listen even when the Activity is paused or destroyed.
+            isRegistered = true;
+            Toast.makeText(this, "Listener activated.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "Listener already active.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // function to unregister the broadcast receiver (Button 2 functionality)
+    private void unregisterBroadcastListener() {
+        if (isRegistered)
+        {
+            // unregister the receiver using the same Application Context
+            getApplicationContext().unregisterReceiver(batteryReceiver);
+            isRegistered = false;
+            Toast.makeText(this, "Listener deactivated.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "Listener is not active.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -61,5 +97,13 @@ public class ChargerInfoActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onStartListening(View view) {
+        registerBroadcastListener();
+    }
+
+    public void onStopListening(View view) {
+        unregisterBroadcastListener();
     }
 }
